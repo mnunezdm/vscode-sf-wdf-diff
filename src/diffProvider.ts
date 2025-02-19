f4301b7645d49e8cf58418d6d9aa1a89d8b82f36
Miguel Núñez Díaz-Montes <mnunezdm@gmail.com>
2025-02-19 22:39:07 +0100

    feat: order nodes before comparing

--- src/diffProvider.ts
+++ src/diffProvider.ts
@@ -28,7 +28,8 @@ export class DiffProvider implements vscode.TextDocumentContentProvider {
       const jsonArrayString = document.getText()
 
       const jsonObject = this.parseJsonArray(jsonArrayString)
-      return JSON.stringify(jsonObject, null, 2)
+      const sortedJson = this.sortAlphabetically(jsonObject)
+      return JSON.stringify(sortedJson, null, 2)
     } catch (error) {
       return 'Error: ' + error
     }
@@ -40,5 +41,27 @@ export class DiffProvider implements vscode.TextDocumentContentProvider {
       (data) => Boolean(data) && data !== '{}',
     )[0]
     return JSON.parse(nonNullNode)
+  }
+
+  private sortAlphabetically(value: any): any {
+    const self = this
+
+    if (Array.isArray(value)) {
+      return value
+        .map((v) => self.sortAlphabetically(v))
+        .sort((a, b) => {
+          const aStr = JSON.stringify(a)
+          const bStr = JSON.stringify(b)
+          return aStr < bStr ? -1 : aStr > bStr ? 1 : 0
+        })
+    } else if (value && typeof value === 'object') {
+      return Object.keys(value)
+        .sort()
+        .reduce((acc, key) => {
+          acc[key] = self.sortAlphabetically(value[key])
+          return acc
+        }, {} as any)
+    }
+    return value
   }
 }
